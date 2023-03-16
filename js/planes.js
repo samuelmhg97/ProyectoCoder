abdd7de7dde471d98132f0948117dbb45a7fe241
Samuel Herrera <samuelmhg97@gmail.com>
2023-03-16 20:04:38 -0300

    Pusheando nuevamente espero valga

--- js/planes.js
+++ js/planes.js
@@ -24,25 +24,32 @@ const users = [
 
 const Courses = [
     {
-        nomadas: {
-
+        nomadas: [
+        {
+            id: 1,
+            nombre: "Bases legales y que necesitas para solicitar visa laboral en mas de 20 paises",
+            price: 999,
+            tutor: "Joshua Herrera",
+            alias: "NOMACOURSED1"
         },
+        {
+            id: 2,
+            nombre: "Mentorias de como adaptarte en otros, paises que necesitas hacer y que necesitas buscar a la hora de viajar a otro pais",
+            price: 1500,
+            tutor: "Joshua Herrera",
+            alias: "NOMACOURSED2"
+        }
+    ]},{
         freelancers: {
             
         }
     }
 ]
-const anonimo = "anonimo"
-const comprador = "comprador"
+// const anonimo = "anonimo"
+// const comprador = "comprador"
 
-let jobState;
-
-const nomada = "nomada" 
-const freelancer = "freelancer"
-const ninguno = "ninguno"
-
-let course1 = false
-let course2 = false
+// let course1 = false
+// let course2 = false
 
 let nacionality = "Venezuela"
 
@@ -55,7 +62,7 @@ function userValidation(confirmation)  {
     }
 }
 
-const logIn = function(user, password) {
+const logIn = function() {
     if(account) {
             let userAttempts = 1
             while(userAttempts <= 3) {
@@ -76,7 +83,6 @@ const logIn = function(user, password) {
                     if(passwordIntro === user.password) {
                         alert(`Bienvenido/a ${user.name}`)
                         user.logState = true
-                        console.log(user)
                         break
                     } else {
                         alert("Clave incorrecta, ingrese nuevamente su clave")
@@ -85,15 +91,13 @@ const logIn = function(user, password) {
                     }
                 } 
                 if(user) {
-                    console.log(user)
                     alert("Ingreso exitoso, estos son los planes que tenemos para tí")
+                    return user
                 }
                 else {
-                    console.log(user)
                     alert("No pudimos validar su usuario, por lo mismo procederemos a continuar como anonimos y al final del recorrido solicitaremos un mail para comunicarnos con usted")
                 }
             } else {
-                console.log(user)
                 alert("No pudimos validar su usuario, por lo mismo procederemos a continuar como anonimos y al final del recorrido solicitaremos un mail para comunicarnos con usted")
             }
     } else {
@@ -102,16 +106,17 @@ const logIn = function(user, password) {
 }
 
 function jobStateValidation() {
+    console.log(user)
     alert("Hola, en Joshua Herrera Group nos enfocamos en los freelancers y nomadas digitales, ofrecemos paquetes de capacitación y cursos")
-    jobState = prompt("Cuentanos, que tipo de trabajador eres, nomada o freelancer? en caso de ser ninguno, no te preocupes ofrecemos una guia para capacitarte en ambos.")
+    let jobState = prompt("Cuentanos, que tipo de trabajador eres, nomada o freelancer? en caso de ser ninguno, no te preocupes ofrecemos una guia para capacitarte en ambos.")
     switch(jobState) {
         case "nomada": 
             alert("Genial, nos encantan esos trabajadores independientes que poseen la visión de recorrer el mundo, o viajar como rutina. Estas son las propuestas que tenemos para tí");
-            jobState = nomada;
+            user.status = jobState
             break;
         case "freelancer":
             alert("Genial, es asombroso ese trabajador autónomo que ofreces sus servicios independientemente del lugar donde cumpla con sus responsabilidades contraídas.Estas son las propuestas que tenemos para tí");
-            jobState = freelancer;
+            user.status = jobState;
             break
         default:
             alert("No pasa nada, Estos son las propuestas que tenemos para tí en las cuales puedes aprender que tipo de trabajador puedes ser")
@@ -119,26 +124,42 @@ function jobStateValidation() {
 }
 
 function purpose() {
-    if(jobState === nomada) {
+    if(user.status === "nomada") {
         alert("Hola querido nomada, selecciona el numero que acompaña al titulo del curso, tranquilo puedes tomar hasta un maximo de 2, sabemos que quieres comprar mas pero pensamos en tu vida social")
         let courseMaxQty = 2;
+        // let findCourses = Courses.find( e => e === user.status)
+        for (findCourse in Courses) {
+
+        }
+        console.log(findCourses)
+        for (let i = 0 ; i < nomadCourse.length; i++) {
+            if(i == nomadCourse.length - 1) {
+                nomadCoursesOffert += `N${i+1} ${nomadCourse[i].nombre} con un precio de $${nomadCourse[i].price}, en caso de no desear ninguno por el momento selecciona 0`
+            }else {
+                nomadCoursesOffert += `N${i+1} ${nomadCourse[i].nombre} con un precio de $${nomadCourse[i].price}, `
+            }
+        }
+        console.log(nomadCourse)
         for (let i = 0; i < courseMaxQty ; i++) {
-            console.log(course1,course2)
-            let course = parseInt(prompt("n°1 Bases legales y que necesitas para solicitar visa laboral en mas de 20 paises costo: $999Ars n°2 Mentorias de como adaptarte en otros, paises que necesitas hacer y que necesitas buscar a la hora de viajar a otro pais costo $1500Ars, en caso de no desear ninguno por el momento selecciona 0"));
+            let nomadCourse = Courses[0].nomadas[0];
+            let nomadCoursesOffert = ""
+
+            let course = parseInt(prompt(nomadCoursesOffert));
             if(course === 0) {
                 break
             }
             else if(course === 1) {
-                if(!course1) {
-                    total += 999;
+                let findCourseInUser = user.Courses.find( e => e.alias === nomadCourse[0].alias)
+                if(!findCourseInUser) {
+                    total += nomadCourse[0].price;
                     alert("Curso añadido con exito!")
+                    user.courses.push(nomadCourse[0])
                     let confirmation = prompt("deseas añadir otro curso?, si o no?")
-                    course1 = true
-                    if (confirmation == "si" && !course2) {
+                    if (confirmation == "si") {
                         continue
                     } else if(confirmation == "no"){
                         return total
-                    } else if(confirmation == "si" && course2) {
+                    } else if(confirmation == "si" && (user.courses.length > 2)) {
                         alert("tranquilo nomada, sabemos que quieres ir por todo, pero a veces es necesario tomar ciertos descansos, como te dijimos antes el maximo de cursos es de 2, nos preocupamos por tu salud")
                         return total
                     } else {
@@ -149,16 +170,17 @@ function purpose() {
                 } else alert("ya añadiste este curso")
             }
             else if(course === 2) {
-                if(!course2) {
-                    total += 1500;
+                let findCourseInUser = user.Courses.find( e => e.alias === nomadCourse[1].alias)
+                if(!findCourseInUser) {
+                    total += nomadCourse[1].price;
                     alert("Curso añadido con exito!")
+                    user.courses.push(nomadCourse[1])
                     let confirmation = prompt("deseas añadir otro curso?, si o no?")
-                    course2 = true
-                    if (confirmation == "si" && !course1) {
+                    if (confirmation == "si") {
                         continue
                     } else if(confirmation == "no"){
                         return total
-                    } else if(confirmation == "si" && course1) {
+                    } else if(confirmation == "si" && (user.courses.length > 2)) {
                         alert("tranquilo nomada, sabemos que quieres ir por todo, pero a veces es necesario tomar ciertos descansos, como te dijimos antes el maximo de cursos es de 2, nos preocupamos por tu salud")
                         return total
                     } 
@@ -176,66 +198,66 @@ function purpose() {
             }
         }
     } 
-    else if(jobState === freelancer) {
-        alert("Hola querido freelancer, selecciona el numero que acompaña al titulo del curso, tranquilo puedes tomar hasta un maximo de 2, sabemos que quieres comprar mas pero pensamos en tu vida social")
-        let courseMaxQty = 2;
-        for (let i = 0; i < courseMaxQty ; i++) {
-            console.log(course1,course2)
-            let course = parseInt(prompt("n°1 Bases legales y tributarias que necesitas antes de cobrar tus trabajos en el exterior costo: $999Ars n°2 Que necesitas saber y en que plataforma debes de buscar tus proximas ofertas laborales costo $1500Ars, en caso de no desear ninguno por el momento selecciona 0"));
-            if(course === 0) {
-                break
-            }
-            else if(course === 1) {
-                if(!course1) {
-                    total += 999;
-                    alert("Curso añadido con exito!")
-                    let confirmation = prompt("deseas añadir otro curso?, si o no?")
-                    course1 = true
-                    if (confirmation == "si" && !course2) {
-                        continue
-                    } else if(confirmation == "no"){
-                        return total
-                    } else if(confirmation == "si" && course2) {
-                        alert("tranquilo freelancer, sabemos que quieres ir por todo, pero a veces es necesario tomar ciertos descansos, como te dijimos antes el maximo de cursos es de 2, nos preocupamos por tu salud")
-                        return total
-                    } else {
-                        do {
-                            confirmation = prompt("Comando Invalido, deseas añadir otro curso?, si o no ")
-                        } while(confirmation !== "si" && confirmation !== "no")
-                    }
-                } else alert("ya añadiste este curso")
-            }
-            else if(course === 2) {
-                if(!course2) {
-                    total += 1500;
-                    alert("Curso añadido con exito!")
-                    let confirmation = prompt("deseas añadir otro curso?, si o no?")
-                    course2 = true
-                    if (confirmation == "si" && !course1) {
-                        continue
-                    } else if(confirmation == "no"){
-                        return total
-                    } else if(confirmation == "si" && course1) {
-                        alert("tranquilo nomada, sabemos que quieres ir por todo, pero a veces es necesario tomar ciertos descansos, como te dijimos antes el maximo de cursos es de 2, nos preocupamos por tu salud")
-                        return total
-                    } 
-                    else {
-                        do {
-                            confirmation = prompt("Comando Invalido, deseas añadir otro curso?, si o no ")
-                        } while(confirmation !== "si" && confirmation !== "no")
-                    }
-                } else alert("ya añadiste este curso")
-            }
-            else {
-                do {
-                    course = parseInt(prompt("Accion invalida por favor selecciona un numero que corresponda"))
-                } while(course < 0 && course > 2)
-            }
-        }
-    } 
-    else {
-        alert("estamos trabajando en ello, te contactaremos proximamente")
-    }
+    // else if(user.status === "freelancer") {
+    //     alert("Hola querido freelancer, selecciona el numero que acompaña al titulo del curso, tranquilo puedes tomar hasta un maximo de 2, sabemos que quieres comprar mas pero pensamos en tu vida social")
+    //     let courseMaxQty = 2;
+    //     for (let i = 0; i < courseMaxQty ; i++) {
+    //         console.log(course1,course2)
+    //         let course = parseInt(prompt("n°1 Bases legales y tributarias que necesitas antes de cobrar tus trabajos en el exterior costo: $999Ars n°2 Que necesitas saber y en que plataforma debes de buscar tus proximas ofertas laborales costo $1500Ars, en caso de no desear ninguno por el momento selecciona 0"));
+    //         if(course === 0) {
+    //             break
+    //         }
+    //         else if(course === 1) {
+    //             if(!course1) {
+    //                 total += 999;
+    //                 alert("Curso añadido con exito!")
+    //                 let confirmation = prompt("deseas añadir otro curso?, si o no?")
+    //                 course1 = true
+    //                 if (confirmation == "si" && !course2) {
+    //                     continue
+    //                 } else if(confirmation == "no"){
+    //                     return total
+    //                 } else if(confirmation == "si" && course2) {
+    //                     alert("tranquilo freelancer, sabemos que quieres ir por todo, pero a veces es necesario tomar ciertos descansos, como te dijimos antes el maximo de cursos es de 2, nos preocupamos por tu salud")
+    //                     return total
+    //                 } else {
+    //                     do {
+    //                         confirmation = prompt("Comando Invalido, deseas añadir otro curso?, si o no ")
+    //                     } while(confirmation !== "si" && confirmation !== "no")
+    //                 }
+    //             } else alert("ya añadiste este curso")
+    //         }
+    //         else if(course === 2) {
+    //             if(!course2) {
+    //                 total += 1500;
+    //                 alert("Curso añadido con exito!")
+    //                 let confirmation = prompt("deseas añadir otro curso?, si o no?")
+    //                 course2 = true
+    //                 if (confirmation == "si" && !course1) {
+    //                     continue
+    //                 } else if(confirmation == "no"){
+    //                     return total
+    //                 } else if(confirmation == "si" && course1) {
+    //                     alert("tranquilo nomada, sabemos que quieres ir por todo, pero a veces es necesario tomar ciertos descansos, como te dijimos antes el maximo de cursos es de 2, nos preocupamos por tu salud")
+    //                     return total
+    //                 } 
+    //                 else {
+    //                     do {
+    //                         confirmation = prompt("Comando Invalido, deseas añadir otro curso?, si o no ")
+    //                     } while(confirmation !== "si" && confirmation !== "no")
+    //                 }
+    //             } else alert("ya añadiste este curso")
+    //         }
+    //         else {
+    //             do {
+    //                 course = parseInt(prompt("Accion invalida por favor selecciona un numero que corresponda"))
+    //             } while(course < 0 && course > 2)
+    //         }
+    //     }
+    // } 
+    // else {
+    //     alert("estamos trabajando en ello, te contactaremos proximamente")
+    // }
 }
 
 function totalCart() {
@@ -258,6 +280,7 @@ alert("Hola bienvenido a Joshua Herrera Group, antes de mostrarte nuestras propu
 userValidation()
 logIn()
 jobStateValidation()
-// purpose()
+console.log(user)
+purpose()
 // totalCart()
-alert("El total de tu carrito querido " + jobState + " es de $" + total)
+// alert("El total de tu carrito querido " + jobState + " es de $" + total)
