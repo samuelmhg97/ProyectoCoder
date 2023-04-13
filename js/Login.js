3d1a087d803b316a19585dc0be3d190da70bad94
Samuel Herrera <samuelmhg97@gmail.com>
2023-04-13 19:40:13 -0300

    login done

--- js/Login.js
+++ js/Login.js
@@ -38,7 +38,7 @@ class User {
 function createUser(users) {
     if (inputRegisterUser.value && inputRegisterPassword.value && inputRegister2Password.value && inputRegisterStatus.value) { 
     if(inputRegisterPassword.value === inputRegister2Password.value) {
-        let findUser = users.find((e) => e.name === inputRegisterUser.value)
+        let findUser = users.find((e) => e.username === inputRegisterUser.value)
         if (findUser) {
             Toast.fire({
                 icon: 'error',
@@ -48,6 +48,7 @@ function createUser(users) {
             const user = new User(inputRegisterUser.value, inputRegisterPassword.value, inputRegisterStatus.value)
             users.push(user)
             user.setId(users)
+            localStorage.setItem("UsersList", JSON.stringify(user))
             Toast.fire({
                 icon: 'success',
                 title: 'Usuario Creado con exito, seras redirigido a nuestra pagina de productos'
@@ -69,14 +70,11 @@ function createUser(users) {
     }
 }
 
-function saveUserStorage(array) {
-    localStorage.setItem("UsersList", JSON.stringify(array))
-}
-
-function loginUser(users, loginText) {
+function loginUser() {
+    const localStorageUsers = JSON.parse(localStorage.getItem("UsersList"))
     if(inputLoginUser.value && inputLoginPassword.value) {
-        let findUser = users.find((e) => e.name === inputRegisterUser.value)
-        if (findUser) {
+        let findUser = users.find((e) => e.username === inputLoginUser.value)
+        if(findUser) {
             if (findUser.password === inputLoginPassword.value) {
                 findUser.logStatus = true
                 Toast.fire({
@@ -92,18 +90,65 @@ function loginUser(users, loginText) {
                     title: 'Contraseña Invalida'
                 })
             } 
-                
-        }  else {
-            Toast.fire({
-                icon: 'error',
-                title: 'Usuario Invalido'
-            })
+        }
+        else if(localStorageUsers) {
+            users = localStorageUsers
+            let findUser = (localStorageUsers.username === inputLoginUser.value)
+            if (findUser) {
+                let findPassword = (localStorageUsers.password === inputLoginPassword.value)
+                if (findPassword) {
+                    localStorageUsers.logStatus = true
+                    Toast.fire({
+                        icon: 'success',
+                        title: 'Inicio de sesion exitoso, seras redirigido'
+                    })
+                    setTimeout(() => {
+                        window.location.assign("./Planes.html")
+                    }, 4000)
+                }  else {
+                    Toast.fire({
+                        icon: 'error',
+                        title: 'Contraseña Invalida'
+                    })
+                } 
+                    
+            }  else {
+                Toast.fire({
+                    icon: 'error',
+                    title: 'Usuario Invalido'
+                })
+            }
+        } else{
+            let findUser = users.find((e) => e.username === inputLoginUser.value)
+            if (findUser) {
+                if (findUser.password === inputLoginPassword.value) {
+                    findUser.logStatus = true
+                    Toast.fire({
+                        icon: 'success',
+                        title: 'Inicio de sesion exitoso, seras redirigido'
+                    })
+                    setTimeout(() => {
+                        window.location.assign("./Planes.html")
+                    }, 4000)
+                }  else {
+                    Toast.fire({
+                        icon: 'error',
+                        title: 'Contraseña Invalida'
+                    })
+                } 
+                    
+            }  else {
+                Toast.fire({
+                    icon: 'error',
+                    title: 'Usuario Invalido'
+                })
+            }
         }
     } else {
-        Toast.fire({
-            icon: 'error',
-            title: 'Porfavor Rellenar los campos    '
-        })
+    Toast.fire({
+        icon: 'error',
+        title: 'Porfavor Rellenar los campos    '
+    })
     }
 }
 const Toast = Swal.mixin({
@@ -119,91 +164,20 @@ const Toast = Swal.mixin({
 })
 
 const getUsers = async function(e) {
-    e.preventDefault()
     let response = await fetch("../js/users.json")
-    let users = await response.json()
-    createUser(users)
-    saveUserStorage(users)
+    users = await response.json()
     console.log(users)
 };
-// btnRegister.onclick = (e) => {
-//     e.preventDefault()
-//     createUser(users, confirmRegister);
-//     saveUserStorage(users)
-//     console.log(users)
-// }
-btnRegister.onclick = getUsers
 
-btnLogin.onclick = (e) => {
+btnRegister.onclick = async (e) => {
     e.preventDefault()
-    loginUser(users, confirmLogin)
+    await getUsers()
+    createUser(users, confirmRegister);
+    console.log(users)
 }
-// function userValidation(confirmation)  {
-//     confirmation= prompt("Es usted un usuario de Joshua Herrera Group?")
-//     if(confirmation ==="si") {
-//         account = true
-//     } else {
-//         console.log(account)
-//     }
-// }
 
-// const logIn = function() {
-//     if(account) {
-//             let userAttempts = 1
-//             while(userAttempts <= 3) {
-//                 userIntro = prompt("Ingrese por favor su usuario (todo en minusculas porfavor)")
-//                 user = users.find((e) => e.name === userIntro)
-//                 console.log(user)
-//                 if(user) break
-//                 else {
-//                     alert("Usuario incorrecto, ingrese nuevamente su usuario")
-//                     userAttempts += 1
-//                     continue
-//                 }
-//             }
-//             if(user) {
-//                 let passwordAttempts = 1
-//                 while(passwordAttempts <= 3) {
-//                     passwordIntro = prompt("Ingrese por favor su clave")
-//                     if(passwordIntro === user.password) {
-//                         alert(`Bienvenido/a ${user.name}`)
-//                         user.logState = true
-//                         break
-//                     } else {
-//                         alert("Clave incorrecta, ingrese nuevamente su clave")
-//                         passwordAttempts += 1
-//                         continue
-//                     }
-//                 } 
-//                 if(user) {
-//                     alert("Ingreso exitoso, estos son los planes que tenemos para tí")
-//                     return user
-//                 }
-//                 else {
-//                     alert("No pudimos validar su usuario, por lo mismo procederemos a continuar como anonimos y al final del recorrido solicitaremos un mail para comunicarnos con usted")
-//                 }
-//             } else {
-//                 alert("No pudimos validar su usuario, por lo mismo procederemos a continuar como anonimos y al final del recorrido solicitaremos un mail para comunicarnos con usted")
-//             }
-//     } else {
-//         alert("Procederemos a continuar como anonimos y al final del recorrido solicitaremos un mail para comunicarnos con usted")
-//     }
-// }
-
-// function jobStateValidation() {
-//     console.log(user)
-//     alert("Hola, en Joshua Herrera Group nos enfocamos en los freelancers y nomadas digitales, ofrecemos paquetes de capacitación y cursos")
-//     let jobState = prompt("Cuentanos, que tipo de trabajador eres, nomada o freelancer? en caso de ser ninguno, no te preocupes ofrecemos una guia para capacitarte en ambos.")
-//     switch(jobState) {
-//         case "nomada": 
-//             alert("Genial, nos encantan esos trabajadores independientes que poseen la visión de recorrer el mundo, o viajar como rutina. Estas son las propuestas que tenemos para tí");
-//             user.status = jobState
-//             break;
-//         case "freelancer":
-//             alert("Genial, es asombroso ese trabajador autónomo que ofreces sus servicios independientemente del lugar donde cumpla con sus responsabilidades contraídas.Estas son las propuestas que tenemos para tí");
-//             user.status = jobState;
-//             break
-//         default:
-//             alert("No pasa nada, Estos son las propuestas que tenemos para tí en las cuales puedes aprender que tipo de trabajador puedes ser")
-//     }
-// }
+btnLogin.onclick = async (e) => {
+    e.preventDefault()
+        await getUsers()
+    loginUser()
+}
--- js/planes.js
+++ js/planes.js
@@ -1,209 +1,81 @@
-let user;
-let account = false;
-let total = 0;
-const users = [
-    {   
-        id: 1,
-        name: "Samu",
-        password: "Admin",
-        status: "Nomada",
-        role: "admin",
-        logState: false,
-        courses: []
-    },
-    {
-        id:2,
-        name: "Balti",
-        password: "SamuelSeMereceUnOptimo",
-        status:"",
-        role: "comprador",
-        logState: false,
-        courses:[]
-    }
-];
 
-const Courses = [
-        {
-            id: 1,
-            nombre: "Bases legales y que necesitas para solicitar visa laboral en mas de 20 paises",
-            price: 999,
-            tutor: "Joshua Herrera",
-            type: "nomada"
-        },
-        {
-            id: 2,
-            nombre: "Mentorias de como adaptarte en otros, paises que necesitas hacer y que necesitas buscar a la hora de viajar a otro pais",
-            price: 1500,
-            tutor: "Joshua Herrera",
-            type: "nomada"
-        },{
-            id: 3,
-            nombre: "n°1 Bases legales y tributarias que necesitas antes de cobrar tus trabajos en el exterior",
-            price: 999,
-            tutor: "Joshua Herrera",
-            type: "freelancer"
-        }, {
-            id: 4,
-            nombre: "Que necesitas saber y en que plataforma debes de buscar tus proximas ofertas laborales",
-            price: 1500,
-            tutor: "Joshua Herrera",
-            type: "freelancer"
+const
 
-        }
 
-]
-// const anonimo = "anonimo"
-// const comprador = "comprador"
+// function purpose() {
+//     if(user.status === "nomada") {
+//         alert("Hola querido nomada, selecciona el numero que acompaña al titulo del curso, tranquilo puedes tomar hasta un maximo de 2, sabemos que quieres comprar mas pero pensamos en tu vida social")
+//         let courseMaxQty = 2;
+//         let findCourses = Courses.filter( e => e.type === user.status)
 
-// let course1 = false
-// let course2 = false
-
-let nacionality = "Venezuela"
-
-// function userValidation(confirmation)  {
-//     confirmation= prompt("Es usted un usuario de Joshua Herrera Group?")
-//     if(confirmation ==="si") {
-//         account = true
-//     } else {
-//         console.log(account)
-//     }
-// }
-
-// const logIn = function() {
-//     if(account) {
-//             let userAttempts = 1
-//             while(userAttempts <= 3) {
-//                 userIntro = prompt("Ingrese por favor su usuario (todo en minusculas porfavor)")
-//                 user = users.find((e) => e.name === userIntro)
-//                 console.log(user)
-//                 if(user) break
-//                 else {
-//                     alert("Usuario incorrecto, ingrese nuevamente su usuario")
-//                     userAttempts += 1
-//                     continue
-//                 }
+//         console.log(findCourses)
+//         let nomadCoursesOffert=""
+//         for (let i = 0 ; i < findCourses.length; i++) {
+//             if(i == findCourses.length - 1) {
+//                 nomadCoursesOffert += `N${i+1} ${findCourses[i].nombre} con un precio de $${findCourses[i].price}, en caso de no desear ninguno por el momento selecciona 0`
+//             }else {
+//                 nomadCoursesOffert += `N${i+1} ${findCourses[i].nombre} con un precio de $${findCourses[i].price}, `
 //             }
-//             if(user) {
-//                 let passwordAttempts = 1
-//                 while(passwordAttempts <= 3) {
-//                     passwordIntro = prompt("Ingrese por favor su clave")
-//                     if(passwordIntro === user.password) {
-//                         alert(`Bienvenido/a ${user.name}`)
-//                         user.logState = true
-//                         break
-//                     } else {
-//                         alert("Clave incorrecta, ingrese nuevamente su clave")
-//                         passwordAttempts += 1
+//         }
+//         console.log(nomadCoursesOffert)
+//         for (let i = 0; i < courseMaxQty ; i++) {
+//             let nomadCourse = Courses[0].nomadas[0];
+//             let nomadCoursesOffert = ""
+
+//             let course = parseInt(prompt(nomadCoursesOffert));
+//             if(course === 0) {
+//                 break
+//             }
+//             else if(course === 1) {
+//                 let findCourseInUser = user.Courses.find( e => e.alias === nomadCourse[0].alias)
+//                 if(!findCourseInUser) {
+//                     total += nomadCourse[0].price;
+//                     alert("Curso añadido con exito!")
+//                     user.courses.push(nomadCourse[0])
+//                     let confirmation = prompt("deseas añadir otro curso?, si o no?")
+//                     if (confirmation == "si") {
 //                         continue
+//                     } else if(confirmation == "no"){
+//                         return total
+//                     } else if(confirmation == "si" && (user.courses.length > 2)) {
+//                         alert("tranquilo nomada, sabemos que quieres ir por todo, pero a veces es necesario tomar ciertos descansos, como te dijimos antes el maximo de cursos es de 2, nos preocupamos por tu salud")
+//                         return total
+//                     } else {
+//                         do {
+//                             confirmation = prompt("Comando Invalido, deseas añadir otro curso?, si o no ")
+//                         } while(confirmation !== "si" && confirmation !== "no")
 //                     }
-//                 } 
-//                 if(user) {
-//                     alert("Ingreso exitoso, estos son los planes que tenemos para tí")
-//                     return user
-//                 }
-//                 else {
-//                     alert("No pudimos validar su usuario, por lo mismo procederemos a continuar como anonimos y al final del recorrido solicitaremos un mail para comunicarnos con usted")
-//                 }
-//             } else {
-//                 alert("No pudimos validar su usuario, por lo mismo procederemos a continuar como anonimos y al final del recorrido solicitaremos un mail para comunicarnos con usted")
+//                 } else alert("ya añadiste este curso")
 //             }
-//     } else {
-//         alert("Procederemos a continuar como anonimos y al final del recorrido solicitaremos un mail para comunicarnos con usted")
-//     }
-// }
-
-// function jobStateValidation() {
-//     console.log(user)
-//     alert("Hola, en Joshua Herrera Group nos enfocamos en los freelancers y nomadas digitales, ofrecemos paquetes de capacitación y cursos")
-//     let jobState = prompt("Cuentanos, que tipo de trabajador eres, nomada o freelancer? en caso de ser ninguno, no te preocupes ofrecemos una guia para capacitarte en ambos.")
-//     switch(jobState) {
-//         case "nomada": 
-//             alert("Genial, nos encantan esos trabajadores independientes que poseen la visión de recorrer el mundo, o viajar como rutina. Estas son las propuestas que tenemos para tí");
-//             user.status = jobState
-//             break;
-//         case "freelancer":
-//             alert("Genial, es asombroso ese trabajador autónomo que ofreces sus servicios independientemente del lugar donde cumpla con sus responsabilidades contraídas.Estas son las propuestas que tenemos para tí");
-//             user.status = jobState;
-//             break
-//         default:
-//             alert("No pasa nada, Estos son las propuestas que tenemos para tí en las cuales puedes aprender que tipo de trabajador puedes ser")
-//     }
-// }
-
-function purpose() {
-    if(user.status === "nomada") {
-        alert("Hola querido nomada, selecciona el numero que acompaña al titulo del curso, tranquilo puedes tomar hasta un maximo de 2, sabemos que quieres comprar mas pero pensamos en tu vida social")
-        let courseMaxQty = 2;
-        let findCourses = Courses.filter( e => e.type === user.status)
-
-        console.log(findCourses)
-        let nomadCoursesOffert=""
-        for (let i = 0 ; i < findCourses.length; i++) {
-            if(i == findCourses.length - 1) {
-                nomadCoursesOffert += `N${i+1} ${findCourses[i].nombre} con un precio de $${findCourses[i].price}, en caso de no desear ninguno por el momento selecciona 0`
-            }else {
-                nomadCoursesOffert += `N${i+1} ${findCourses[i].nombre} con un precio de $${findCourses[i].price}, `
-            }
-        }
-        console.log(nomadCoursesOffert)
-        for (let i = 0; i < courseMaxQty ; i++) {
-            let nomadCourse = Courses[0].nomadas[0];
-            let nomadCoursesOffert = ""
-
-            let course = parseInt(prompt(nomadCoursesOffert));
-            if(course === 0) {
-                break
-            }
-            else if(course === 1) {
-                let findCourseInUser = user.Courses.find( e => e.alias === nomadCourse[0].alias)
-                if(!findCourseInUser) {
-                    total += nomadCourse[0].price;
-                    alert("Curso añadido con exito!")
-                    user.courses.push(nomadCourse[0])
-                    let confirmation = prompt("deseas añadir otro curso?, si o no?")
-                    if (confirmation == "si") {
-                        continue
-                    } else if(confirmation == "no"){
-                        return total
-                    } else if(confirmation == "si" && (user.courses.length > 2)) {
-                        alert("tranquilo nomada, sabemos que quieres ir por todo, pero a veces es necesario tomar ciertos descansos, como te dijimos antes el maximo de cursos es de 2, nos preocupamos por tu salud")
-                        return total
-                    } else {
-                        do {
-                            confirmation = prompt("Comando Invalido, deseas añadir otro curso?, si o no ")
-                        } while(confirmation !== "si" && confirmation !== "no")
-                    }
-                } else alert("ya añadiste este curso")
-            }
-            else if(course === 2) {
-                let findCourseInUser = user.Courses.find( e => e.alias === nomadCourse[1].alias)
-                if(!findCourseInUser) {
-                    total += nomadCourse[1].price;
-                    alert("Curso añadido con exito!")
-                    user.courses.push(nomadCourse[1])
-                    let confirmation = prompt("deseas añadir otro curso?, si o no?")
-                    if (confirmation == "si") {
-                        continue
-                    } else if(confirmation == "no"){
-                        return total
-                    } else if(confirmation == "si" && (user.courses.length > 2)) {
-                        alert("tranquilo nomada, sabemos que quieres ir por todo, pero a veces es necesario tomar ciertos descansos, como te dijimos antes el maximo de cursos es de 2, nos preocupamos por tu salud")
-                        return total
-                    } 
-                    else {
-                        do {
-                            confirmation = prompt("Comando Invalido, deseas añadir otro curso?, si o no ")
-                        } while(confirmation !== "si" && confirmation !== "no")
-                    }
-                } else alert("ya añadiste este curso")
-            }
-            else {
-                do {
-                    course = parseInt(prompt("Accion invalida por favor selecciona un numero que corresponda"))
-                } while(course < 0 && course > 2)
-            }
-        }
-    } 
+//             else if(course === 2) {
+//                 let findCourseInUser = user.Courses.find( e => e.alias === nomadCourse[1].alias)
+//                 if(!findCourseInUser) {
+//                     total += nomadCourse[1].price;
+//                     alert("Curso añadido con exito!")
+//                     user.courses.push(nomadCourse[1])
+//                     let confirmation = prompt("deseas añadir otro curso?, si o no?")
+//                     if (confirmation == "si") {
+//                         continue
+//                     } else if(confirmation == "no"){
+//                         return total
+//                     } else if(confirmation == "si" && (user.courses.length > 2)) {
+//                         alert("tranquilo nomada, sabemos que quieres ir por todo, pero a veces es necesario tomar ciertos descansos, como te dijimos antes el maximo de cursos es de 2, nos preocupamos por tu salud")
+//                         return total
+//                     } 
+//                     else {
+//                         do {
+//                             confirmation = prompt("Comando Invalido, deseas añadir otro curso?, si o no ")
+//                         } while(confirmation !== "si" && confirmation !== "no")
+//                     }
+//                 } else alert("ya añadiste este curso")
+//             }
+//             else {
+//                 do {
+//                     course = parseInt(prompt("Accion invalida por favor selecciona un numero que corresponda"))
+//                 } while(course < 0 && course > 2)
+//             }
+//         }
+//     } 
     // else if(user.status === "freelancer") {
     //     alert("Hola querido freelancer, selecciona el numero que acompaña al titulo del curso, tranquilo puedes tomar hasta un maximo de 2, sabemos que quieres comprar mas pero pensamos en tu vida social")
     //     let courseMaxQty = 2;
@@ -264,29 +136,30 @@ function purpose() {
     // else {
     //     alert("estamos trabajando en ello, te contactaremos proximamente")
     // }
-}
+// }
 
-function totalCart() {
-    if(jobState) {
-        alert("Genial el total de tus cursos a tomar es de $" + total + " pero, te propongo un juegito, tienes sola una oportunidad para ganar un 75% de descuento, algo asi como una coderbeca pero aun mas genial, bien vamos por ello")
+// function totalCart() {
+//     if(jobState) {
+//         alert("Genial el total de tus cursos a tomar es de $" + total + " pero, te propongo un juegito, tienes sola una oportunidad para ganar un 75% de descuento, algo asi como una coderbeca pero aun mas genial, bien vamos por ello")
 
-        let answer = prompt("¿De que nacionalidad es el creador de este contenido? (escribe el país si o si con la primera letra en mayuscula) a- Argentina  b- Colombia c- Ecuador d- Venezuela e- Ninguna de las anteriores")
+//         let answer = prompt("¿De que nacionalidad es el creador de este contenido? (escribe el país si o si con la primera letra en mayuscula) a- Argentina  b- Colombia c- Ecuador d- Venezuela e- Ninguna de las anteriores")
 
-        if(answer === nacionality) {
-            alert("Es correcto, felicidades! te has ganado un 75% de descuento el total de tu compra, a disfrutarlo")
-            let discount = total * 0.75
-            total = total - discount
-            return total
-        } else {
-            alert("Ups te equivocaste, la respuesta correcta es " + nacionality)
-        }
-    }
-}
-alert("Hola bienvenido a Joshua Herrera Group, antes de mostrarte nuestras propuestas queremos saber si eres ya un usuario, a continuacion aparecerá un espacio de verificacion de usuario, en caso de poseer uno")
+//         if(answer === nacionality) {
+//             alert("Es correcto, felicidades! te has ganado un 75% de descuento el total de tu compra, a disfrutarlo")
+//             let discount = total * 0.75
+//             total = total - discount
+//             return total
+//         } else {
+//             alert("Ups te equivocaste, la respuesta correcta es " + nacionality)
+//         }
+//     }
+// }
+// alert("Hola bienvenido a Joshua Herrera Group, antes de mostrarte nuestras propuestas queremos saber si eres ya un usuario, a continuacion aparecerá un espacio de verificacion de usuario, en caso de poseer uno")
 // userValidation()
 // logIn()
 // jobStateValidation()
-console.log(user)
-purpose()
+// console.log(user)
+// purpose()
 // totalCart()
 // alert("El total de tu carrito querido " + jobState + " es de $" + total)
+
